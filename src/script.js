2275a986c8df28bb9104581e689fed296d6b9809
azim1993 <azimnurul1993@gmail.com>
2023-07-29 22:23:28 +0600

    Heading input value make reactive

--- src/script.js
+++ src/script.js
@@ -1,14 +1,48 @@
 window.addEventListener('load', function() {
     const headingInputCreateBtn = document.getElementById('poster_heading_item_buttom')
-    const headingInput = document.getElementById('heading_input')
+    const headingInputSection = document.getElementById('heading_input_section')
     const headingRemoveBtn = this.document.getElementById('heading_input_remove_button')
+    const headingInput = this.document.getElementById('heading_input')
+    const headingPreview = this.document.getElementById('heading_preview')
 
     headingInputCreateBtn.addEventListener('click', () => {
-        headingInput.classList.remove('hidden')
+        headingInputSection.classList.remove('hidden')
         headingInputCreateBtn.classList.add('hidden')
     })
     headingRemoveBtn.addEventListener('click', () => {
-        headingInput.classList.add('hidden')
+        headingInputSection.classList.add('hidden')
         headingInputCreateBtn.classList.remove('hidden')
+        headingPreview.innerText = null
+        headingInput.value = null
+    })
+
+    const imageInputCreateBtn = document.getElementById('poster_image_item_buttom')
+    const imageInputSection = document.getElementById('image_input_section')
+    const imageRemoveBtn = this.document.getElementById('image_input_remove_button')
+
+    imageInputCreateBtn.addEventListener('click', () => {
+        imageInputSection.classList.remove('hidden')
+        imageInputCreateBtn.classList.add('hidden')
+    })
+    imageRemoveBtn.addEventListener('click', () => {
+        imageInputSection.classList.add('hidden')
+        imageInputCreateBtn.classList.remove('hidden')
+    })
+
+    const descriptionInputCreateBtn = document.getElementById('poster_description_item_buttom')
+    const descriptionInputSection = document.getElementById('description_input_section')
+    const descriptionRemoveBtn = this.document.getElementById('description_input_remove_button')
+
+    descriptionInputCreateBtn.addEventListener('click', () => {
+        descriptionInputSection.classList.remove('hidden')
+        descriptionInputCreateBtn.classList.add('hidden')
+    })
+    descriptionRemoveBtn.addEventListener('click', () => {
+        descriptionInputSection.classList.add('hidden')
+        descriptionInputCreateBtn.classList.remove('hidden')
+    })
+
+    headingInput.addEventListener('keyup', (event) => {
+        headingPreview.innerText = event.target.value
     })
 })
